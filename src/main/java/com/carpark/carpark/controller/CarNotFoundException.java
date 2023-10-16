9a4b1a6645616991cdd6330b486a59bef62b4029
johnnyRobot <beitschalbua@gmx.at>
2023-10-16 14:38:01 +0200

    save

--- src/main/java/com/carpark/carpark/controller/CarNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.carpark.carpark.controller;
-
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class CarNotFoundException extends Exception {
-}
