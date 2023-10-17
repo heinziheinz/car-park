1a85bef554a06a3ddf3f166129738bf0b7d688f1
johnnyRobot <beitschalbua@gmx.at>
2023-10-17 12:03:51 +0200

    added CURD endpoints

--- src/main/java/com/carpark/carpark/controller/UserController.java
+++ src/main/java/com/carpark/carpark/controller/UserController.java
@@ -4,11 +4,10 @@ package com.carpark.carpark.controller;
 import com.carpark.carpark.model.Car;
 import com.carpark.carpark.model.User;
 import com.carpark.carpark.repository.UserRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("users")
@@ -24,4 +23,40 @@ public class UserController {
     List<User> findAll() {
         return userRepository.findAll();
     }
+
+    @GetMapping("/name/{name}")
+    List<User> findAllByType(@PathVariable String name) {
+        return userRepository.findAllByName(name);
+    }
+    @PostMapping
+    User save(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable long id) {
+        userRepository.deleteById(id);
+    }
+
+    @GetMapping("/id/{id}")
+    User findById(@PathVariable long id) throws RescourceNotFoundException {
+        return userRepository.findById(id).orElseThrow(RescourceNotFoundException::new);
+    }
+
+    @PutMapping("/{id}")
+    User update(@PathVariable long id, @RequestBody User updatedUser) throws RescourceNotFoundException {
+        Optional<User> existingUser = userRepository.findById(id);
+
+        if (existingUser.isPresent()) {
+            User user = existingUser.get();
+            user.setName(updatedUser.getName());
+            user.setBirthdate(updatedUser.getBirthdate());
+            user.setAddress(updatedUser.getAddress());
+
+            return userRepository.save(user);
+        } else {
+            throw new RescourceNotFoundException();
+        }
+    }
+
 }
--- src/main/java/com/carpark/carpark/repository/UserRepository.java
+++ src/main/java/com/carpark/carpark/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.carpark.carpark.repository;
 import com.carpark.carpark.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findAllByName(String name);
 }
